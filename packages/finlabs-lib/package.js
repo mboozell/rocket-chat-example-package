9af7b69ee6d1a3e9ce80922e4985f5ee1d15ec8c
Julian Popescu <jpopesculian@gmail.com>
2015-12-21 14:16:30 -0500

    added an email admins function and setting

--- packages/finlabs-lib/package.js
+++ packages/finlabs-lib/package.js
@@ -10,7 +10,8 @@ Package.onUse(function(api) {
 
 	api.use([
 		'coffeescript',
-		'underscore'
+		'underscore',
+		'rocketchat:lib@0.0.1'
 	]);
 
 	api.use([], 'server');
@@ -20,7 +21,10 @@ Package.onUse(function(api) {
 
 	api.addFiles([], ['server', 'client']);
 
-	api.addFiles([], 'server');
+	api.addFiles([
+		"server/settings.coffee",
+		"server/lib/emailAdminsUpdate.coffee"
+	], 'server');
 
 	api.addFiles([], 'client');
 
