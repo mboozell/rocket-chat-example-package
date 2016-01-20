da4d1e0961d3bd965bd3fd838e2972afed94675b
Georgios Diamantopoulos <georgiosd@live.com>
2016-01-20 14:14:41 +0200

    Set up datadog, improvements in upstart and added deployment script

--- deployment/files/config-prod.sh
+++ deployment/files/config-prod.sh
@@ -7,14 +7,15 @@ echo "10.0.0.5 sanglucci-secondary" >> /etc/hosts
 echo "10.0.0.6 sanglucci-arb" >> /etc/hosts
 
 ip=`hostname -I | xargs`
+hostname=`hostname`
 
 setup_upstart_config Web.Development "https:\/\/live.wallstjesus.com" 8080 "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/\$DB?replicaSet=rs0\&readPreference=nearest\&w=majority" "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/local"
-
 setup_nginx_config wallstjesus.com live.wallstjesus.com 8080
+
+# add mongo backup job
 
 cp backup-mongo.sh /storage/apps
 
-hostname=`hostname`
 if [ hostname = "sanglucci-primary" ]; then
 	echo "0 */2 	* * * root /storage/apps/backup-mongo.sh" >> /etc/crontab
 elif [ hostname = "sanglucci-secondary" ] then
