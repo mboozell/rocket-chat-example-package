28f8eedae4f5d3aa21cabdd096f395727f0534fd
Georgios Diamantopoulos <georgiosd@live.com>
2016-01-21 17:35:12 +0200

    Fix bug in upstart template

--- deployment/files/config-prod.sh
+++ deployment/files/config-prod.sh
@@ -9,7 +9,7 @@ echo "10.0.0.6 sanglucci-arb" >> /etc/hosts
 ip=`hostname -I | xargs`
 hostname=`hostname`
 
-setup_upstart_config Web.Development "https:\/\/live.wallstjesus.com" 8080 "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/\$DB?replicaSet=rs0\&readPreference=nearest\&w=majority" "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/local"
+setup_upstart_config Web.Development "https:\/\/live.wallstjesus.com" 8080 "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/SangLucci_Chat-Web_Production?replicaSet=rs0\&readPreference=nearest\&w=majority" "mongodb:\/\/sanglucci-primary:27017,sanglucci-secondary:27017,sanglucci-arb:27017\/local"
 setup_nginx_config wallstjesus.com live.wallstjesus.com 8080
 
 # add mongo backup job
